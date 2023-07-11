d6ce623525436b287949e6a1f376870d9e3b3558
Alexandre Castro <alexandrecotrimcastro@gmail.com>
2023-07-11 20:38:28 -0300

    feat: added user registration with password hash lib

--- Dockerfile
+++ Dockerfile
@@ -4,14 +4,16 @@ WORKDIR /var/www/todo-app
 
 COPY . .
 
-RUN apt update && apt install apache2 -y && apt install libdbi-perl -y && apt install libdbd-mysql-perl -y
+RUN apt update && apt install apache2 -y && apt install libdbi-perl -y \
+    && apt install libdbd-mysql-perl -y && apt install gcc
 
 # Site config file for apache2
 RUN cp ./todo-app.conf /etc/apache2/sites-available/todo-app.conf
 
 # Install Perl required modules
 RUN apt install cpanminus -y && cpan CGI && cpan Text::Template \
-    && cpan CGI::Session && cpan Dotenv && cpan DBI && cpan DBD::mysql
+    && cpan CGI::Session && cpan Dotenv && cpan DBI && cpan DBD::mysql \
+    && cpan Data::Dump && cpan Digest::SHA3 && cpan Crypt::PBKDF2
 
 # Enable modules and site
 RUN a2enmod cgid && a2enmod rewrite && a2ensite todo-app
--- app/Controllers/AuthController.pm
+++ app/Controllers/AuthController.pm
@@ -4,27 +4,53 @@ use strict;
 use warnings;
 use lib "../core/Template";
 use lib "../core/Database";
+use lib "../core";
+use lib "../core/Http";
 use Text::Template;
 use Data::Dumper;
 use DB;
 use Render;
+use Data::Dump qw(dump);
+use Crypt::PBKDF2;
+use Response;
+use Session;
 
-sub sayHello {
-    Render::view(
-        'home',
-        {
-            name => "Hello world!"
-        }
-    );
+sub login {
+    return Render::view('auth/login');
 }
 
-sub hailMVC {
-    Render::view(
-        'home',
-        {
-            name => "Alexandre"
-        }
-    );
+sub attemptLogin {
+    print 'Tentando logar';
+}
+
+sub register {
+    return Render::view('auth/register');
+}
+
+sub attemptRegister {
+    my ( $cgi, $session ) = @_;
+
+    my $pbkdf2   = Crypt::PBKDF2->new();
+    my $password = $cgi->param('password');
+
+    my $hash  = $pbkdf2->generate($password);
+    my $name  = $cgi->param('name');
+    my $email = $cgi->param('email');
+
+    my $sth =
+      DB->getInstance()
+      ->prepare('INSERT INTO users (name, email, password) VALUES (?, ?, ?)');
+
+    $sth->execute( $name, $email, $hash );
+
+    if ( $sth->rows == 1 ) {
+        Response->redirect('/login')
+          ->with( success => 'Successfully registered.' );
+    }
+    else {
+        Response->redirect('/register')
+          ->with( error => 'Error in registration.' );
+    }
 }
 
 1;
